b59169b1f42aea962945a129c34b401c6f6979c3
Matheus <matheusdev20@gmail.com>
2022-05-09 20:12:45 -0300

    folders structure

--- linked_lists/c++/insert_any_position.cpp
+++ linked_lists/c++/insert_any_position.cpp
@@ -32,6 +32,7 @@ void insertPos(Node **current, int pos, int data)
   else
   {
     while (pos--)
+      cout << pos << endl;
     {
       if (pos == 0)
       {
@@ -53,45 +54,46 @@ void insertPos(Node **current, int pos, int data)
 
 // This function prints contents
 // of the linked list
-void printList(struct Node* head)
+void printList(struct Node *head)
 {
-    while (head != NULL) {
-        cout << " " << head->data;
-        head = head->next;
-    }
-    cout << endl;
+  while (head != NULL)
+  {
+    cout << " " << head->data;
+    head = head->next;
+  }
+  cout << endl;
 }
 
 int main()
 {
-    // Creating the list 3->5->8->10
-    Node* head = NULL;
-    head = getNode(3);
-    head->next = getNode(5);
-    head->next->next = getNode(8);
-    head->next->next->next = getNode(10);
- 
-    size = 4;
- 
-    cout << "Linked list before insertion: ";
-    printList(head);
- 
-    int data = 12, pos = 3;
-    insertPos(&head, pos, data);
-    cout << "Linked list after insertion of 12 at position 3: ";
-    printList(head);
- 
-    // front of the linked list
-    data = 1, pos = 1;
-    insertPos(&head, pos, data);
-    cout << "Linked list after insertion of 1 at position 1: ";
-    printList(head);
- 
-    // insertion at end of the linked list
-    data = 15, pos = 7;
-    insertPos(&head, pos, data);
-    cout << "Linked list after insertion of 15 at position 7: ";
-    printList(head);
- 
-    return 0;
+  // Creating the list 3->5->8->10
+  Node *head = NULL;
+  head = getNode(3);
+  head->next = getNode(5);
+  head->next->next = getNode(8);
+  head->next->next->next = getNode(10);
+
+  size = 4;
+
+  cout << "Linked list before insertion: ";
+  printList(head);
+
+  int data = 12, pos = 3;
+  insertPos(&head, pos, data);
+  cout << "Linked list after insertion of 12 at position 3: ";
+  printList(head);
+
+  // front of the linked list
+  data = 1, pos = 1;
+  insertPos(&head, pos, data);
+  cout << "Linked list after insertion of 1 at position 1: ";
+  printList(head);
+
+  // insertion at end of the linked list
+  data = 15, pos = 7;
+  insertPos(&head, pos, data);
+  cout << "Linked list after insertion of 15 at position 7: ";
+  printList(head);
+
+  return 0;
 }
